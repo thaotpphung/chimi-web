638dad9d63e86b8a57c87fe8101df0086ed6199a
Thao Phung <thao.tp.phung@gmail.com>
2024-02-25 18:05:02 -0500

    refactor: update source path for tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '@components',
-        replacement: path.resolve(__dirname, './src/components'),
-      },
-    ],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
