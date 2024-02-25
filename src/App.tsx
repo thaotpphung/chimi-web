1b3eefb5146dd6e2fd184f92ea87d6513777aef0
Thao Phung <thao.tp.phung@gmail.com>
2024-02-25 18:13:13 -0500

    feat(mealplan): add dashboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import { MealPlan } from '@/components/meal-plan';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <MealPlan />
+    </>
+  );
 }
 
 export default App;
